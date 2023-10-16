331ee5a1a079f98d465f61e2108217a52860a016
quinnha <cadetha@gmail.com>
2023-10-15 22:21:39 -0400

    created api endpoint to get satellite location from TLE data

--- src/leo-server-app/src/server.ts
+++ src/leo-server-app/src/server.ts
@@ -1,10 +1,36 @@
 import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
-const port = 3000;
+const cors = require('cors');
+const port = 3001;
+const satellite = require('satellite.js');
+
+// Allow requests from your React app's origin (http://localhost:3000)
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+
+app.use(cors(corsOptions));
+
+// NEUDOSE TLE
+var tleLine1 = '1 99172U          23114.51041667 -.00000031  00000-0 -52816-6 0 00003',
+    tleLine2 = '2 99172 051.6609 234.5214 0004321 232.3001 269.5133 15.50456434000013';  
+      
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, this is Express + TypeScript");
+});
+
+// For more satellite info, check out: https://github.com/shashwatak/satellite-js
+app.get('/getSatelliteInfo', (req, res) => {
+  var satrec = satellite.twoline2satrec(tleLine1, tleLine2);
+  var positionAndVelocity = satellite.propagate(satrec, new Date());
+  
+  var positionEci = positionAndVelocity.position,
+      velocityEci = positionAndVelocity.velocity;
+
+  res.json({positionEci, velocityEci});
+  
 });
 
 app.listen(port, () => {
