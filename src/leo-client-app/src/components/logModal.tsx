1c033afb688f97faa48cbb6d80b767f84f262b9e
Rishi Vaya <rishivaya2@gmail.com>
2023-11-24 18:38:08 -0500

    removed generic import

--- src/leo-client-app/src/components/logModal.tsx
+++ src/leo-client-app/src/components/logModal.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import React from 'react';
 
 
 type Props = {
@@ -26,7 +26,7 @@ const downloadLogs = () => {
 }
 
   return (
-    <React.Fragment>
+    
 
       <Dialog
         open={open}
@@ -49,6 +49,6 @@ const downloadLogs = () => {
           </Button>
         </DialogActions>
       </Dialog>
-    </React.Fragment>
+    
   );
 }
