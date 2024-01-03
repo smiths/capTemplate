6c1d7f8c2293a1d49c689a377889941b083e5e96
quinn ha <cadetha@gmail.com>
2024-01-03 17:13:01 -0500

    removed polar plot from home page

--- src/leo-client-app/src/pages/satellite-passes/index.tsx
+++ src/leo-client-app/src/pages/satellite-passes/index.tsx
@@ -2,7 +2,6 @@
 
 import SatelliteInfo from "@/components/SatelliteInfo";
 import FuturePasses from "@/components/FuturePasses";
-import DetailedDisplay from "@/components/DetailedDisplay";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0/client";
 import { Stack } from "@mui/material";
 import Navbar from "@/components/navbar/Navbar";
@@ -13,7 +12,6 @@ function SatelliteInfoPage() {
       <Navbar />
       <Stack spacing={3} alignItems="center" p={2}>
         <SatelliteInfo />
-        <DetailedDisplay />
         <FuturePasses />
       </Stack>
     </main>
