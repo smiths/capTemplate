177581bf2618c20919aa3ec0814812a2aa460786
Umang-Rajkarnikar <umangrajkarnikar1@gmail.com>
2024-01-15 21:06:52 -0500

    Updated export method of user and command schemas

--- src/leo-server-app/src/models/user.ts
+++ src/leo-server-app/src/models/user.ts
@@ -22,4 +22,6 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+export default User;
