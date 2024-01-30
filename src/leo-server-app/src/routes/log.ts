a48daf79b4c31a1ddd11b9af722881a9f048fa3e
Rishi Vaya <rishivaya2@gmail.com>
2024-01-30 16:09:35 -0500

    made new props for logs endoints

--- src/leo-server-app/src/routes/log.ts
+++ src/leo-server-app/src/routes/log.ts
@@ -4,7 +4,49 @@ const Log = require("../models/log");
 const router = express.Router();
 router.use(express.json());
 
-router.get("/getLogs", async (req: any, res: any) => {
+type GetLogBySatelliteProp = {
+  query: {
+    satelliteId: string;
+  };
+};
+
+type GetLogByScheduleProp = {
+  query: {
+    satelliteId: string;
+    scheduleId: string;
+  };
+};
+
+type GetLogByCommandeProp = {
+  query: {
+    satelliteId: string;
+    commandId: string;
+  };
+};
+
+router.get("/getLogsBySatellite", async (req: GetLogBySatelliteProp, res: any) => {
+  const { satelliteId } = req.query;
+
+  const filter = {
+    satellite: satelliteId,
+  };
+
+  const logs = await Log.find(filter).exec();
+  res.status(201).json({ message: "Fetched logs", logs });
+});
+
+router.get("/getLogsbySchedule", async (req: GetLogByScheduleProp, res: any) => {
+  const { satelliteId } = req.query;
+
+  const filter = {
+    satellite: satelliteId,
+  };
+
+  const logs = await Log.find(filter).exec();
+  res.status(201).json({ message: "Fetched logs", logs });
+});
+
+router.get("/getLogsByCommand", async (req: GetLogByCommandeProp, res: any) => {
   const { satelliteId } = req.query;
 
   const filter = {
