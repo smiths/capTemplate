a1f6b58ee912e9a31fc68521e1ce6cce094652dc
Umang-Rajkarnikar <umangrajkarnikar1@gmail.com>
2024-01-31 01:09:00 -0500

    Added comment for event emitter

--- src/leo-server-app/src/event/satellite.event.ts
+++ src/leo-server-app/src/event/satellite.event.ts
@@ -3,6 +3,9 @@ import { addSchedulesForNext7Days } from "../utils/schedule.utils";
 
 export const SatelliteEventEmitter = new EventEmitter();
 
+/* This event will run anytime a new satellite has been added
+ * It creates overpass schedulesfor the next 7 days
+ */
 SatelliteEventEmitter.on(
   "satelliteCreated",
   (satelliteId: string, noradId: string) => {
