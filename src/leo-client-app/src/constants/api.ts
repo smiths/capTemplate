82f6d10e98288d0ca441d3aaf03103eb8250d22c
Umang-Rajkarnikar <umangrajkarnikar1@gmail.com>
2024-02-06 20:14:49 -0500

    Added api calls to send and get commands for schedule

--- src/leo-client-app/src/constants/api.ts
+++ src/leo-client-app/src/constants/api.ts
@@ -17,3 +17,33 @@ export const updateOperatorRole = async (userId: string, data: any) => {
   );
   return operator.data;
 };
+
+export const sendCommandSchedule = async (
+  userId: string,
+  scheduleId: string,
+  satelliteId: string,
+  commands: string[]
+) => {
+  const body = {
+    userId,
+    scheduleId,
+    satelliteId,
+    commands,
+  };
+
+  const res = await axios.post(
+    `${BACKEND_URL}/schedule/createBatchScheduledCommand`,
+    body
+  );
+  return res.data;
+};
+
+export const getCommandsBySchedule = async (scheduleId: string) => {
+  const res = await axios.get(`${BACKEND_URL}/schedule/getCommandsBySchedule`, {
+    params: {
+      scheduleId,
+      limit: 100,
+    },
+  });
+  return res.data;
+};
