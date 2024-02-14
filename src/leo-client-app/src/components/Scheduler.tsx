02aa26b9f7ae44382f8328a2747c0a2d030d16e7
dhruvcheemakurti <spidey.rock7@gmail.com>
2024-02-14 02:45:36 -0500

    latest schedule ui

--- src/leo-client-app/src/components/Scheduler.tsx
+++ src/leo-client-app/src/components/Scheduler.tsx
@@ -1,3 +1,4 @@
+
 import '../components/Scheduler.css'; // Import the CSS styles
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
