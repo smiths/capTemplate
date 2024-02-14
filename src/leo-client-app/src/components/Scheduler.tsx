cf959c46b492f8f0b7a42f2036a21e3418205cd0
dhruvcheemakurti <spidey.rock7@gmail.com>
2024-02-14 11:05:42 -0500

    working on rendering schedules

--- src/leo-client-app/src/components/Scheduler.tsx
+++ src/leo-client-app/src/components/Scheduler.tsx
@@ -1,4 +1,4 @@
-
+import React, { useEffect, useState } from "react";
 import '../components/Scheduler.css'; // Import the CSS styles
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
@@ -6,8 +6,8 @@ import EditSchedulePage from "@/components/EditSchedules";
 import { useRouter } from "next/router";
 import { sendCommandSchedule } from "@/constants/api";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import React, { useEffect, useState } from "react";
 import ViewScheduleCard from "./ViewScheduleCard";
+//import Scheduler from "@/components/EditSchedules";
 
 
 interface Command {
@@ -15,11 +15,115 @@ interface Command {
 }
 
 interface Schedule {
-  date: string;
-  startTime: string;
-  endTime: string;
-  commands: Command[];
+  id: string;
+  startDate: string;
+  endDate: string;
+  satelliteId: string;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
 }
+
+type Props = {
+  noradId: string;
+};
+
+function formatDate(dateString: string) {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+  const date = new Date(dateString);
+  return date.toLocaleDateString(undefined, options);
+}
+
+function formatTimeRange(startTime: string, endTime: string) {
+  const startDate = new Date(startTime);
+  const endDate = new Date(endTime);
+
+  const startHours = startDate.getHours();
+  const startMinutes = startDate.getMinutes().toString().padStart(2, "0");
+  const endHours = endDate.getHours();
+  const endMinutes = endDate.getMinutes().toString().padStart(2, "0");
+
+
+  const formattedStartTime = startHours + ":" + startMinutes;
+  const formattedEndTime = endHours + ":" + endMinutes;
+
+  return `${formattedStartTime} - ${formattedEndTime}`;
+}
+// const UpcomingSchedules = ({ noradId }: Props) => {
+//   // TODO: Dynamically get satelliteId from somewhere
+//   const satelliteId = "655acd63d122507055d3d2ea";
+//   const [schedules1, setSchedules] = useState<Schedule[]>([]);
+//   const [isLoading, setIsLoading] = useState<boolean>(false);
+//   const [scheduleCommands, setScheduleCommands] = useState<{
+//     [scheduleId: string]: string[];
+//   }>({});
+
+
+//   const fetchSchedules = (satelliteId: string) => {
+//     setIsLoading(true); // Set loading state to true when starting to fetch
+//     fetch(
+//       `http://localhost:3001/schedule/getSchedulesBySatellite?satelliteId=${satelliteId}&page=1&limit=100`
+//     )
+//       .then((response) => response.json())
+//       .then((data) => {
+//         if (data.schedules) {
+//           // Map through each schedule and create a new schedule object
+//           const transformedSchedules = data.schedules.map((schedule: any) => ({
+//             id: schedule._id,
+//             startDate: schedule.startDate,
+//             endDate: schedule.endDate,
+//             satelliteId: schedule.satelliteId,
+//             status: schedule.status,
+//             createdAt: schedule.createdAt,
+//             updatedAt: schedule.updatedAt,
+//           }));
+//           // Set the transformed schedules
+//           setSchedules(transformedSchedules);
+//           // After setting schedules, fetch commands for each schedule
+//           transformedSchedules.forEach((schedule: Schedule) => {
+//             fetchCommandsPerScheduleAndUpdateState(schedule.id);
+//           });
+//         } else {
+//           setSchedules([]);
+//         }
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching satellite schedules:", error);
+//       })
+//       .finally(() => setIsLoading(false)); // Reset loading state regardless of result
+//   };
+
+//   const fetchCommandsPerScheduleAndUpdateState = (scheduleId: string) => {
+//     fetch(
+//       `http://localhost:3001/schedule/getCommandsBySchedule?scheduleId=${scheduleId}`
+//     )
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setScheduleCommands((prevCommands) => ({
+//           ...prevCommands,
+//           [scheduleId]: data.commands ?? [],
+//         }));
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching schedule commands:", error);
+//       });
+//   };
+
+//   useEffect(() => {
+//     fetchSchedules(satelliteId);
+//   }, [satelliteId]);
+
+
+
+
+
+
+
+
 
 // Mock data
 const schedules: Schedule[] = [
@@ -110,13 +214,9 @@ const ScheduleComponent: React.FC<{ schedule: Schedule }> = ({ schedule }) => {
           {/* button for edit schedules */}
         </div>
         <button className="add-schedule">+</button>
-        
-        
-  
       </div>
   )};
-
-  export default SchedulesPage;
+export default SchedulesPage;
 
 
 
