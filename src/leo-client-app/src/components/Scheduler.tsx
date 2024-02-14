14232cd7ebfba96400a977f125c8c4e8a44e231a
dhruvcheemakurti <spidey.rock7@gmail.com>
2024-02-14 16:41:10 -0500

    some commit

--- src/leo-client-app/src/components/Scheduler.tsx
+++ src/leo-client-app/src/components/Scheduler.tsx
@@ -7,6 +7,14 @@ import { useRouter } from "next/router";
 import { sendCommandSchedule } from "@/constants/api";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import ViewScheduleCard from "./ViewScheduleCard";
+import {
+  Box,
+  Card,
+  CardContent,
+  CircularProgress,
+  Grid,
+  Stack,
+} from "@mui/material";
 //import Scheduler from "@/components/EditSchedules";
 
 
@@ -53,172 +61,244 @@ function formatTimeRange(startTime: string, endTime: string) {
 
   return `${formattedStartTime} - ${formattedEndTime}`;
 }
-// const UpcomingSchedules = ({ noradId }: Props) => {
-//   // TODO: Dynamically get satelliteId from somewhere
-//   const satelliteId = "655acd63d122507055d3d2ea";
-//   const [schedules1, setSchedules] = useState<Schedule[]>([]);
-//   const [isLoading, setIsLoading] = useState<boolean>(false);
-//   const [scheduleCommands, setScheduleCommands] = useState<{
-//     [scheduleId: string]: string[];
-//   }>({});
+const UpcomingSchedules = ({ noradId }: Props) => {
+  // TODO: Dynamically get satelliteId from somewhere
+  const satelliteId = "655acd63d122507055d3d2ea";
+  const [schedules1, setSchedules] = useState<Schedule[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [scheduleCommands, setScheduleCommands] = useState<{
+    [scheduleId: string]: string[];
+  }>({});
 
 
-//   const fetchSchedules = (satelliteId: string) => {
-//     setIsLoading(true); // Set loading state to true when starting to fetch
-//     fetch(
-//       `http://localhost:3001/schedule/getSchedulesBySatellite?satelliteId=${satelliteId}&page=1&limit=100`
-//     )
-//       .then((response) => response.json())
-//       .then((data) => {
-//         if (data.schedules) {
-//           // Map through each schedule and create a new schedule object
-//           const transformedSchedules = data.schedules.map((schedule: any) => ({
-//             id: schedule._id,
-//             startDate: schedule.startDate,
-//             endDate: schedule.endDate,
-//             satelliteId: schedule.satelliteId,
-//             status: schedule.status,
-//             createdAt: schedule.createdAt,
-//             updatedAt: schedule.updatedAt,
-//           }));
-//           // Set the transformed schedules
-//           setSchedules(transformedSchedules);
-//           // After setting schedules, fetch commands for each schedule
-//           transformedSchedules.forEach((schedule: Schedule) => {
-//             fetchCommandsPerScheduleAndUpdateState(schedule.id);
-//           });
-//         } else {
-//           setSchedules([]);
-//         }
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching satellite schedules:", error);
-//       })
-//       .finally(() => setIsLoading(false)); // Reset loading state regardless of result
-//   };
+  const fetchSchedules = (satelliteId: string) => {
+    setIsLoading(true); // Set loading state to true when starting to fetch
+    fetch(
+      `http://localhost:3001/schedule/getSchedulesBySatellite?satelliteId=${satelliteId}&page=1&limit=100`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.schedules) {
+          // Map through each schedule and create a new schedule object
+          const transformedSchedules = data.schedules.map((schedule: any) => ({
+            id: schedule._id,
+            startDate: schedule.startDate,
+            endDate: schedule.endDate,
+            satelliteId: schedule.satelliteId,
+            status: schedule.status,
+            createdAt: schedule.createdAt,
+            updatedAt: schedule.updatedAt,
+          }));
+          // Set the transformed schedules
+          setSchedules(transformedSchedules);
+          // After setting schedules, fetch commands for each schedule
+          transformedSchedules.forEach((schedule: Schedule) => {
+            fetchCommandsPerScheduleAndUpdateState(schedule.id);
+          });
+        } else {
+          setSchedules([]);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching satellite schedules:", error);
+      })
+      .finally(() => setIsLoading(false)); // Reset loading state regardless of result
+  };
 
-//   const fetchCommandsPerScheduleAndUpdateState = (scheduleId: string) => {
-//     fetch(
-//       `http://localhost:3001/schedule/getCommandsBySchedule?scheduleId=${scheduleId}`
-//     )
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setScheduleCommands((prevCommands) => ({
-//           ...prevCommands,
-//           [scheduleId]: data.commands ?? [],
-//         }));
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching schedule commands:", error);
-//       });
-//   };
+  const fetchCommandsPerScheduleAndUpdateState = (scheduleId: string) => {
+    fetch(
+      `http://localhost:3001/schedule/getCommandsBySchedule?scheduleId=${scheduleId}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setScheduleCommands((prevCommands) => ({
+          ...prevCommands,
+          [scheduleId]: data.commands ?? [],
+        }));
+      })
+      .catch((error) => {
+        console.error("Error fetching schedule commands:", error);
+      });
+  };
 
-//   useEffect(() => {
-//     fetchSchedules(satelliteId);
-//   }, [satelliteId]);
+  useEffect(() => {
+    fetchSchedules(satelliteId);
+  }, [satelliteId]);
 
+  // const ScheduleComponent: React.FC<{ schedule: Schedule }> = ({ schedule }) => {
+  //   const router = useRouter();
+  //   return (
+  //     <div className="schedule">
+  //       <div className="schedule-header">
+  //         {schedule.startDate} 
+  //         {/* <br> </br> */}
+  //         {schedule.startTime} - {schedule.endTime}
+  //       </div>
+  //       <ul className="commands">
+  //         {schedule.commands.map((command, index) => (
+  //           <li key={index}>{command.name}</li>
+  //         ))}
+  //       </ul>
 
-
-
-
-
-
-
-
-// Mock data
-const schedules: Schedule[] = [
-  {
-    date: "Jan 9 2024",
-    startTime: "9:10 AM",
-    endTime: "9:12 AM",
-    commands: [
-      { name: "Setup" },
-      { name: "Command 1" },
-      { name: "Command 2" },
-      { name: "Command 3" },
-      { name: "Teardown" },
-    ],
-  },
-  {
-    date: "Jan 9 2024",
-    startTime: "9:10 AM",
-    endTime: "9:12 AM",
-    commands: [
-      { name: "Setup" },
-      { name: "Command 1" },
-      { name: "Command 2" },
-      { name: "Command 3" },
-      { name: "Teardown" },
-    ],
-  },
-  {
-    date: "Jan 9 2024",
-    startTime: "9:10 AM",
-    endTime: "9:12 AM",
-    commands: [
-      { name: "Setup" },
-      { name: "Command 1" },
-      { name: "Command 2" },
-      { name: "Command 3" },
-      { name: "Teardown" },
-    ],
-  },
-  // ... more schedules
-];
-
-const ScheduleComponent: React.FC<{ schedule: Schedule }> = ({ schedule }) => {
-  const router = useRouter();
-  return (
-    <div className="schedule">
-      <div className="schedule-header">
-        {schedule.date} 
-        {/* <br> </br> */}
-        {schedule.startTime} - {schedule.endTime}
-      </div>
-      <ul className="commands">
-        {schedule.commands.map((command, index) => (
-          <li key={index}>{command.name}</li>
-        ))}
-      </ul>
-
-      <button onClick={()=>{router.push('/edit-schedules')}} > Edit Schedules </button>
-      
-    </div>
-  )};
-
-  const SchedulesPage: React.FC = () => {
-    return (
-      <div className="schedules-page">
-        {/* this is for satellite name title; for now its being hardcoded later on it will be dynamic (another pr) */}
-        <header> 
-          <div className='satellite-name'>
-            Satellite Name
-          </div>
-        </header>
-        {/* All schedules for that particular satellite */}
-        <header>
-          <div className='all-schedules'>
-              All Schedules
-            </div>
-        </header>
-        <header> 
-          <div className='schedule-queue'> 
-        Schedule Queue
-          </div> 
-        </header>
+  //       <button onClick={()=>{router.push('/edit-schedules')}} > Edit Schedules </button>
+        
+  //     </div>
+  //   )};
+          
+  // const SchedulesPage: React.FC = () => {
+  //   return (
+  //     <div className="schedules-page">
+  //       {/* this is for satellite name title; for now its being hardcoded later on it will be dynamic (another pr) */}
+  //       <header> 
+  //         <div className='satellite-name'>
+  //           Satellite Name
+  //         </div>
+  //       </header>
+  //       {/* All schedules for that particular satellite */}
+  //       <header>
+  //         <div className='all-schedules'>
+  //             All Schedules
+  //           </div>
+  //       </header>
+  //       <header> 
+  //         <div className='schedule-queue'> 
+  //       Schedule Queue
+  //         </div> 
+  //       </header>
     
-        <div className="schedule-queue">
-          {schedules.map((schedule, index) => (
-            <ScheduleComponent key={index} schedule={schedule} />
-          ))}
-          {/* button for edit schedules */}
-        </div>
-        <button className="add-schedule">+</button>
-      </div>
-  )};
-export default SchedulesPage;
+  //       <div className="schedule-queue">
+  //         {schedules1.map((schedule, index) => (
+  //           <ScheduleComponent key={index} schedule={schedule} />
+  //         ))}
+  //         {/* button for edit schedules */}
+  //       </div>
+  //       <button className="add-schedule">+</button>
+  //     </div>
+  // )};
+  
+  return (
+    <div className="upcomingSchedulesBox">
+      <Stack alignItems="flex-start" spacing={1}>
+        <p className="headerBox">Schedule Queue</p>
+        {isLoading ? (
+          <Box className="loadingBox">
+            <CircularProgress />
+          </Box>
+        ) : (
+          <Grid
+            className="futureSchedulesBox"
+            container
+            spacing={2}
+            sx={{
+              display: "flex",
+              flexWrap: "nowrap",
+              overflowX: "auto",
+              maxWidth: "98vw", // Use 100vw to ensure it considers the full viewport width
+              boxSizing: "border-box",
+              "& .MuiGrid-item": {
+                flex: "0 0 auto",
+              },
+              mx: -2,
+            }}
+          >
+            {schedules1 &&
+              schedules1.map((schedule, index) => (
+                <Grid item key={index}>
+                  <Card
+                    sx={{
+                      minWidth: 150,
+                      margin: 0.5,
+                      backgroundColor:
+                        "var(--material-theme-sys-light-inverse-on-surface)",
+                      cursor: "pointer",
+                      borderRadius: 3,
+                    }}
+                  >
+                    <CardContent>
+                      <Stack spacing={0}>
+                        <p className="cardTitle">
+                          {formatDate(schedule.startDate)}
+                        </p>
+
+                        <p className="cardSubtitle">
+                          {formatTimeRange(
+                            schedule.startDate,
+                            schedule.endDate
+                          )}
+                        </p>
+
+                        <>
+                          {scheduleCommands[schedule.id] &&
+                          scheduleCommands[schedule.id].length > 0 ? (
+                            scheduleCommands[schedule.id].map(
+                              (commandObj: any, cmdIndex) => (
+                                // Render each command in a separate <p> tag
+                                <p key={cmdIndex} className="cardSubtitle">
+                                  {commandObj.command}
+                                </p>
+                              )
+                            )
+                          ) : (
+                            <p className="cardSubtitle">No commands</p>
+                          )}
+                        </>
+                      </Stack>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))}
+          </Grid>
+        )}
+      </Stack>
+    </div>
+  );
+
+            };
+export default UpcomingSchedules;
 
 
+
+
+// // Mock data
+// const schedules: Schedule[] = [
+//   {
+//     date: "Jan 9 2024",
+//     startTime: "9:10 AM",
+//     endTime: "9:12 AM",
+//     commands: [
+//       { name: "Setup" },
+//       { name: "Command 1" },
+//       { name: "Command 2" },
+//       { name: "Command 3" },
+//       { name: "Teardown" },
+//     ],
+//   },
+//   {
+//     date: "Jan 9 2024",
+//     startTime: "9:10 AM",
+//     endTime: "9:12 AM",
+//     commands: [
+//       { name: "Setup" },
+//       { name: "Command 1" },
+//       { name: "Command 2" },
+//       { name: "Command 3" },
+//       { name: "Teardown" },
+//     ],
+//   },
+//   {
+//     date: "Jan 9 2024",
+//     startTime: "9:10 AM",
+//     endTime: "9:12 AM",
+//     commands: [
+//       { name: "Setup" },
+//       { name: "Command 1" },
+//       { name: "Command 2" },
+//       { name: "Command 3" },
+//       { name: "Teardown" },
+//     ],
+//   },
+//   // ... more schedules
+// ];
 
 
 // const Scheduler: React.FC = () => {
