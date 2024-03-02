bacdaffe2de0414851d8d52f5bd1d987c7b98378
DiamondAhuja <ahujadiamond@gmail.com>
2024-03-02 01:35:57 -0500

    updated table stylings and alignment

--- src/leo-client-app/src/components/logs.tsx
+++ src/leo-client-app/src/components/logs.tsx
@@ -14,9 +14,8 @@ import {
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import LogDialog from "./logModal";
-import "./styles/logs.css"
+import "./styles/logs.css";
 import moment from "moment";
-
 
 const Logs: React.FC = () => {
   // TODO: Dynamicall get satelliteId from somewhere
@@ -52,69 +51,68 @@ const Logs: React.FC = () => {
   }, [satelliteId]);
 
   return (
-    <Stack className="stack" alignItems="center" spacing={3} py={5}>
-      <Typography variant="h5">Logs</Typography>
-      <TableContainer
-        component={Paper}
-        className="table-container"
-      >
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell className="table-cell">
-                Created At
-              </TableCell>
-              <TableCell className="table-cell">
-                Last Updated
-              </TableCell>
-              <TableCell className="table-cell">
-                Logs
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {logs &&
-              logs?.map((data: any, index: number) => (
-                <TableRow
-                  key={data._id + index}
-                  className="table-row"
-                >
-                  <TableCell
-                    className="table-cell"
-                    component="th"
-                    scope="row"
-                  >
-                    {moment
-                      .utc(data.createdAt)
-                      .local()
-                      .format("DD/MM/YYYY HH:mm:ss")}
-                  </TableCell>
-                  <TableCell className="table-cell">
-                    {moment
-                      .utc(data.updatedAt)
-                      .local()
-                      .format("DD/MM/YYYY HH:mm:ss")}
-                  </TableCell>
-                  <TableCell className="table-cell">
-                    <Button
-                      variant="text"
-                      className="button"
-                      onClick={() => handleLogOpen(data)}
+    <div className="logsBox">
+      <Stack className="stack" alignItems="flex-start" spacing={3} py={8}>
+        <Typography variant="h5">Logs</Typography>
+        <TableContainer
+          component={Paper}
+          sx={{
+            maxWidth: 800,
+            background: "var(--material-theme-sys-light-primary-fixed)",
+            '& .MuiTableCell-root': {  // This targets all TableCell components
+              borderBottom: "2px solid black",
+            },
+          }}
+        >
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell className="table-cell">Created At</TableCell>
+                <TableCell className="table-cell">Last Updated</TableCell>
+                <TableCell className="table-cell">Logs</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {logs &&
+                logs?.map((data: any, index: number) => (
+                  <TableRow key={data._id + index} className="table-row">
+                    <TableCell
+                      className="table-cell"
+                      component="th"
+                      scope="row"
                     >
-                      Show Logs
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <LogDialog
-        open={openLog}
-        logData={logData}
-        handleClose={handleLogClose}
-      />
-    </Stack>
+                      {moment
+                        .utc(data.createdAt)
+                        .local()
+                        .format("DD/MM/YYYY HH:mm:ss")}
+                    </TableCell>
+                    <TableCell className="table-cell">
+                      {moment
+                        .utc(data.updatedAt)
+                        .local()
+                        .format("DD/MM/YYYY HH:mm:ss")}
+                    </TableCell>
+                    <TableCell className="table-cell">
+                      <Button
+                        variant="text"
+                        className="button"
+                        onClick={() => handleLogOpen(data)}
+                      >
+                        Show Logs
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <LogDialog
+          open={openLog}
+          logData={logData}
+          handleClose={handleLogClose}
+        />
+      </Stack>
+    </div>
   );
 };
 
