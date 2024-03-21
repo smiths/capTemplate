17e728f8bfccccef9060f97f5c65246d7fa12a45
Umang-Rajkarnikar <umangrajkarnikar1@gmail.com>
2024-03-20 20:38:40 -0400

    Merged socket and express servers

--- src/leo-server-app/src/app.ts
+++ src/leo-server-app/src/app.ts
@@ -1,8 +1,21 @@
 import express, { Express, Request, Response } from "express";
 import * as dotenv from "dotenv";
-
+import * as http from "http";
 import bodyParser from "body-parser";
+import { WebSocket } from "ws";
 
+// import routes
+const messageHandler = require("./messageHandler");
+const usersRoute = require("./routes/user");
+const sateliiteUserRoute = require("./routes/satelliteUser");
+const { router: satelliteRoute } = require("./routes/satellite");
+const scheduleRoute = require("./routes/schedule");
+const logRoute = require("./routes/log");
+const pingRoute = require("./routes/ping");
+
+const cors = require("cors");
+
+// Load env
 dotenv.config({
   path: `.env.${
     (process.env.NODE_ENV === "development" ? "local" : process.env.NODE_ENV) ||
@@ -13,15 +26,7 @@ dotenv.config({
 
 const app: Express = express();
 
-const cors = require("cors");
-
-const usersRoute = require("./routes/user");
-const sateliiteUserRoute = require("./routes/satelliteUser");
-const { router: satelliteRoute } = require("./routes/satellite");
-const scheduleRoute = require("./routes/schedule");
-const logRoute = require("./routes/log");
-const pingRoute = require("./routes/ping");
-
+// Configure cors
 const corsOptions = {
   origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
@@ -31,7 +36,7 @@ app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Connect routes to app
+// -------- Express HTTP routes --------
 app.use("/satelliteUser", sateliiteUserRoute);
 app.use("/users", usersRoute);
 app.use("/satellite", satelliteRoute);
@@ -43,4 +48,25 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello, this is Express + TypeScript");
 });
 
-module.exports = app;
+// Create HTTP server instance
+const AppServer = http.createServer(app);
+
+// -------- Websocket connection --------
+const wss = new WebSocket.Server({ server: AppServer });
+
+wss.on("connection", function connection(ws: any) {
+  console.log("----------------------------------------");
+  console.log(`Client connected`);
+  console.log("----------------------------------------");
+  messageHandler.setClientSocket(ws);
+
+  ws.on("message", function message(data: any) {
+    console.log(`${data}`);
+  });
+
+  ws.on("close", function close() {
+    console.log("Client disconnected");
+  });
+});
+
+module.exports = AppServer;
