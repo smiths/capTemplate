b3b33bba304a3ef23bdf6a7d93076b23253697dc
Umang-Rajkarnikar <umangrajkarnikar1@gmail.com>
2024-03-20 21:46:13 -0400

    Created scheduler terminal component

---
+++ src/leo-client-app/src/components/SchedulerTerminal.tsx
@@ -0,0 +1,63 @@
+import { sendCommandToForwarder } from "@/constants/api";
+import { useGetValidCommands } from "@/constants/hooks";
+import { Box, Stack } from "@mui/material";
+import { useRouter } from "next/router";
+import { ReactTerminal } from "react-terminal";
+
+const SchedulerTerminal = () => {
+  const router = useRouter();
+
+  const scheduleId = router.query?.scheduleId?.toString() ?? "";
+  const satelliteId = router.query?.satId?.toString() ?? "";
+  const userId = "65a8181f36ea10b4366e1dd9";
+  const validCommands = useGetValidCommands(satelliteId, userId);
+
+  const delay = (ms: any) => new Promise((res) => setTimeout(res, ms));
+
+  const sendCommand = async (command: string) => {
+    try {
+      const res = await sendCommandToForwarder(
+        userId,
+        scheduleId,
+        satelliteId,
+        // TODO: change
+        "credits"
+      );
+      return res;
+    } catch (error) {}
+  };
+
+  return (
+    <Stack maxWidth={1000}>
+      <Box sx={{ width: "100%", height: 300 }}>
+        <ReactTerminal
+          commands={validCommands.data?.record[0].validCommands}
+          // showControlBar={false}
+          themes={{
+            "my-custom-theme": {
+              themeBGColor: "#272B36",
+              themeToolbarColor: "#DBDBDB",
+              themeColor: "#FFFEFC",
+              themePromptColor: "#a917a8",
+            },
+          }}
+          theme="my-custom-theme"
+          defaultHandler={async (request: any) => {
+            const isValid = validCommands.data?.record[0]?.validCommands.some(
+              (cmd: string) => cmd === request
+            );
+            if (!isValid) {
+              return "Invalid command sequence";
+            }
+
+            const res = await sendCommand(request);
+            console.log(res);
+            return res?.output ?? "Error";
+          }}
+        />
+      </Box>
+    </Stack>
+  );
+};
+
+export default SchedulerTerminal;
