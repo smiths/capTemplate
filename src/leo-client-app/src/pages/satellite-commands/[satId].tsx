7625e8e6c6eb4a788469444f1e61bee6095044a6
DiamondAhuja <ahujadiamond@gmail.com>
2024-03-30 01:02:00 -0400

    Add SatelliteCommandsPage component

--- src/leo-client-app/src/pages/satellite-commands/[satId].tsx
+++ src/leo-client-app/src/pages/satellite-commands/[satId].tsx
@@ -1,1 +1,28 @@
 "use client";
+
+import { withPageAuthRequired } from "@auth0/nextjs-auth0/client";
+import Navbar from "@/components/navbar/Navbar";
+import "../../styles.css";
+import { Stack } from "@mui/material";
+import SatelliteCommands from "@/components/SatelliteCommands";
+
+function SatelliteCommandsPage() {
+  return (
+    <main>
+      <Stack
+        sx={{
+          width: "100%",
+          minHeight: "100vh",
+          backgroundColor: "var(--material-theme-black)",
+        }}
+        alignItems="center"
+        p={2}
+      >
+        <Navbar />
+        <SatelliteCommands />
+      </Stack>
+    </main>
+  );
+}
+
+export default withPageAuthRequired(SatelliteCommandsPage);
