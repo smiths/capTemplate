67f7e9385ca5f58ef2227f7c512b74e9908fdb90
DiamondAhuja <ahujadiamond@gmail.com>
2024-03-30 01:20:56 -0400

    added satellite name

--- src/leo-client-app/src/components/SatelliteCommands.tsx
+++ src/leo-client-app/src/components/SatelliteCommands.tsx
@@ -8,27 +8,31 @@ import axios from "axios";
 import SatelliteName from "./SatelliteName";
 
 const SatelliteCommands: React.FC = () => {
-    const router = useRouter();
-    const satId = router.query?.satId?.toString() ?? "655acd63d122507055d3d2ea";
-    const [satelliteName, setSatelliteName] = useState<string>();
+  const router = useRouter();
+  const satId = router.query?.satId?.toString() ?? "655acd63d122507055d3d2ea";
+  const [satelliteName, setSatelliteName] = useState<string>();
 
-    const fetchData = async () => {
-        try {
-          const res = await axios.get(`${BACKEND_URL}/satellite/getSatellite`, {
-            params: { satelliteId: satId },
-          });
-          setSatelliteName(res.data.satellite.name);
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
-      };
+  const fetchData = async () => {
+    try {
+      const res = await axios.get(`${BACKEND_URL}/satellite/getSatellite`, {
+        params: { satelliteId: satId },
+      });
+      setSatelliteName(res.data.satellite.name);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
-    return (
-        <div>
-            <SatelliteName satelliteName={satelliteName as string}/>
-            <h1>Satellite Commands</h1>
-        </div>
-    );
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <SatelliteName satelliteName={satelliteName as string} />
+      <h1>Satellite Commands</h1>
+    </div>
+  );
 };
 
 export default SatelliteCommands;
